52d1018f589325702945105b8a09f5af4b6d84f0
Chuanwang WANG <chaunecy@outlook.com>
2019-12-06 10:45:31 +0800

    feat(first commit): create project

---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by cw on 12/6/19.
+//
+
+#ifndef PWDTOOLS_MAIN_H
+#define PWDTOOLS_MAIN_H
+
+#include <string>
+#include "include/json.hpp"
+#endif //PWDTOOLS_MAIN_H
---
+++ tools/LenCount.cpp
@@ -0,0 +1,6 @@
+//
+// Created by cw on 12/6/19.
+//
+
+
+std::string
